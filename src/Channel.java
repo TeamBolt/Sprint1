2da2ca33c172441d260f73e26c09b6176538d332
Kevari <Kevari@Kevari-PC>
2015-03-01 16:03:12 -0400

    Added some methods to the Channel class.

--- src/Channel.java
+++ src/Channel.java
@@ -2,4 +2,24 @@
 public class Channel {
 	Sensor sensor;
 	Boolean enabled;
+	
+	public Channel(){
+		enabled = false;
+	}
+	
+	public void toggle(){
+		enabled = !enabled;
+	}
+	
+	public void connectSensor(Sensor s){
+		sensor = s;
+	}
+	
+	/**
+	 * Get a signal from a sensor and trigger an event on the timing System.
+	 */
+	public void trigger(){
+		if(!enabled) return;
+		// Use some method by the connected sensor
+	}
 }
