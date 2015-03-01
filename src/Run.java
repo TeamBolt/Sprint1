ce82ae96ffde508674b3373c2c89d96c82216500
Chris Harmon <cjharmon@uwm.edu>
2015-03-01 14:46:45 -0600

    Added the triggering, canceling, and dnf functionality to RunGroup and Run, and gave RunGroup it's constructor which sets up default values.

--- src/Run.java
+++ src/Run.java
@@ -1,18 +1,26 @@
 
 public class Run {
 	long startTime;
-	long stopTime;
+	long finishTime;
 	int bibNum;
 	int runNum;
 	String state;
 	
+	/**
+	 * Creates a run for the:
+	 * @param int run 	run number, associated with a RunGroup.
+	 * @param int bib	bib number, associated with a Competitor.
+	 */
 	public Run( int run, int bib ) {
 		bibNum = bib;
 		runNum = run;
 	}
 	
+	/**
+	 * @return elapsed time, or 0 if the run isn't finished.
+	 */
 	public long getElapsed() {
-		if ( state.equals("finished") ) return stopTime - startTime;
+		if ( state.equals("finished") ) return finishTime - startTime;
 		return 0;
 	}
 	
--- src/RunGroupInd.java
+++ src/RunGroupInd.java
@@ -8,26 +8,92 @@ public class RunGroupInd implements RunGroup{
 
 	public int runNum;
 	public int startChannel;
-	public int stopChannel;
+	public int finishChannel;
 	public LinkedBlockingQueue<Run> startQueue;
 	public LinkedBlockingQueue<Run> finishQueue;
 	public HashSet<Run> completedRuns;
 	
+	/**
+	 * This sets up the RunGroup to the default values, and gives it it's run number.
+	 */
+	public RunGroupInd() {
+		// This is 1 if there are no RunGroups in the archive, and increments thereafter.
+		runNum = ChronoTimer.archive.size() + 1;
+		
+		// Defaults.
+		startChannel = 1;
+		finishChannel = 2;
+		
+		// Instantiations.
+		startQueue = new LinkedBlockingQueue<Run>();
+		finishQueue = new LinkedBlockingQueue<Run>();
+		completedRuns = new HashSet<Run>();
+	}
+	
+	/**
+	 * Starts and Finishes runs if the channel supplied is one we are interested in
+	 * and there is at least one run in the appropriate start or finish queue.
+	 * 
+	 * @param int channel		The channel which was triggered.
+	 * @param long timestamp	The time at which the trigger occured.
+	 */
 	@Override
 	public void trigger(int channel, long timestamp) {
-		// TODO Auto-generated method stub
+		
+		if ( channel == startChannel && !startQueue.isEmpty() ) {
+			// Start channel was triggered, the run is off!
+			Run current = startQueue.poll();
+			current.startTime = timestamp;
+			current.state = "inProgress";
+			finishQueue.add(current);
+		} else if ( channel == finishChannel && !finishQueue.isEmpty()) {
+			// Finish channel triggered, the run is completed.
+			Run current = finishQueue.poll();
+			current.finishTime = timestamp;
+			current.state = "finished";
+			completedRuns.add(current);
+		}
 		
 	}
 
+	/**
+	 * Cancels the run which would be next to finish (if there is one) and moves
+	 * it back to the front of the startQueue.
+	 */
 	@Override
 	public void cancel() {
-		// TODO Auto-generated method stub
+		// Cancel only makes sense if there is a run waiting to finish.
+		if ( finishQueue.isEmpty() ) return;
+		
+		Run current = finishQueue.poll();
+		current.state = "waiting";
+		
+		if ( !startQueue.isEmpty() ) {
+			// If there were people waiting to start, we need to budge in line.
+			LinkedBlockingQueue<Run> tempQueue = new LinkedBlockingQueue<Run>();
+			tempQueue.add(current);
+			tempQueue.addAll(startQueue);
+			startQueue = tempQueue;
+		} else {
+			// Otherwise we just move the run back into the startQueue.
+			startQueue.add(current);
+		}
+		
 		
 	}
 
+	/**
+	 * DNFs the run that would be first to finish, gives it state "dnf" but no
+	 * finishTime and moves it into completedRuns.
+	 */
 	@Override
 	public void dnf() {
-		// TODO Auto-generated method stub
+		// DNF only makes sense if there is a run waiting to finish.
+		if ( finishQueue.isEmpty() ) return;
+		
+		Run current = finishQueue.poll();
+		current.state = "dnf";
+		completedRuns.add(current);
 		
 	}
 
