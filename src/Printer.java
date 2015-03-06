53618b04bc5c436a300b6dfec137519c1d898ece
Chris Harmon <cjharmon@uwm.edu>
2015-03-06 14:38:02 -0600

    Added a log to the printer so that we can test if messages were printed.

--- src/Printer.java
+++ src/Printer.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Printer class represents both the printer and the console.
  * Prints to the console always, and to the printer when it isOn.
@@ -9,8 +11,10 @@
 public class Printer {
 
 	public static boolean isOn = false;
+	public static ArrayList<String> log = new ArrayList<String>();
 	
 	public static void print(String output) {
+		log.add(output);
 		System.out.println(output);
 		
 		if ( isOn == true ) {
