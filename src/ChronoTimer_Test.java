cb94fcab5c737a21875ebf520c644bd33a6bb752
Chris Harmon <cjharmon@uwm.edu>
2015-03-06 11:26:09 -0600

    Added a couple of test cases to get the idea.

--- src/ChronoTimer_Test.java
+++ src/ChronoTimer_Test.java
@@ -6,8 +6,29 @@ import org.junit.Test;
 public class ChronoTimer_Test {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testReadCommand() {	
+		// Test that commands don't work before the system is on.
+		ChronoTimer.readCommand(0, "START");
+		assertEquals("Command was found in eventLog (shouldn't have been).", 0, ChronoTimer.eventLog.size());
+		
+		// Clear the eventLog between each case.
+		ChronoTimer.eventLog.clear();
+		
+		// Test that command is in eventLog.
+		ChronoTimer.readCommand(0, "ON");
+		assertEquals("No command found in eventLog.", 1, ChronoTimer.eventLog.size());
+		assertEquals("Incorrect command found in eventLog.", "18:00:00.0" + "	" + "ON", ChronoTimer.eventLog.get(0));
+		
+		// Clear the eventLog between each case.
+		ChronoTimer.eventLog.clear();
+		
+		ChronoTimer.readCommand(0, "TOGGLE");
+		assertEquals("Command was found in eventLog (shouldn't have been).", 0, ChronoTimer.eventLog.size());
+		ChronoTimer.readCommand(0, "TOGGLE A");
+		assertEquals("Command was found in eventLog (shouldn't have been).", 0, ChronoTimer.eventLog.size());
+		ChronoTimer.readCommand(0, "TOGGLE 1");
+		assertEquals("No command found in eventLog.", 1, ChronoTimer.eventLog.size());
+		assertEquals("Incorrect command found in eventLog.", "18:00:00.0" + "	" + "TOGGLE 1", ChronoTimer.eventLog.get(0));
 	}
 
 }
