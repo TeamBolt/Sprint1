9fbafe158fc2d858f7d17098ca91f5ae3135ca9b
Chris Harmon <cjharmon@uwm.edu>
2015-03-08 01:04:48 -0600

    Added the setUp function to TestCommands, tweaked, and added some comments, look for notes -> ::NOTE::

--- src/TestCommands.java
+++ src/TestCommands.java
@@ -1,37 +1,43 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestCommands {
 
+	// This
+	@Before
+	public void setUp() throws Exception {
+		ChronoTimer.isOn = false;
+		ChronoTimer.eventType = null;
+		ChronoTimer.current = null;
+		ChronoTimer.archive = new ArrayList<RunGroup>();
+		ChronoTimer.channels = new ArrayList<Channel>();
+		ChronoTimer.eventLog = new ArrayList<String>();
+	}
 	
 	@Test
 	public void testOn() {
-		ChronoTimer ct = new ChronoTimer();
-		assertFalse("New ChronoTimer should not default to on", ct.isOn);
-		assertTrue("Run group should not be set yet", ct.eventType==null);
-		assertNull("Current should be null", ct.current);
+		assertFalse("New ChronoTimer should not default to on", ChronoTimer.isOn);
+		assertTrue("Run group should not be set yet", ChronoTimer.eventType==null);
+		assertNull("Current should be null", ChronoTimer.current);
 		
-
-		long timestamp = SystemTimer.getTime();
-		
-		
-		ChronoTimer x = new ChronoTimer();
-		x.readCommand(timestamp, "ON");
-		assertTrue("ChronoTimer's On command didn't add 8 channels", x.channels.size()==8);
-		assertTrue("ChronoTimer isn't on, but still ran throu On.execute", x.isOn);
-		assertTrue("ChronoTimer's run group should be 'IND'", x.eventType.equals("IND"));
-		assertNotNull("Current should not be null", x.current);
+		ChronoTimer.readCommand(0, "ON");
+		assertTrue("ChronoTimer's On command didn't add 8 channels", ChronoTimer.channels.size()==8);
+		assertTrue("ChronoTimer isn't on, but still ran throu On.execute", ChronoTimer.isOn);
+		assertTrue("ChronoTimer's run group should be 'IND'", ChronoTimer.eventType.equals("IND"));
+		assertNotNull("Current should not be null", ChronoTimer.current);
 	}
 
 	
 	@Test
 	public void testOff(){
-		ChronoTimer t = new ChronoTimer();
 		long timestamp = SystemTimer.getTime();
-		t.readCommand(timestamp, "ON");
-		t.readCommand(timestamp+=10000, "OFF");
+		ChronoTimer.readCommand(timestamp, "ON");
+		ChronoTimer.readCommand(timestamp+=10000, "OFF");
 			
 		
 		
@@ -88,28 +94,33 @@ public class TestCommands {
 	
 	@Test
 	public void testStart(){
-		ChronoTimer c = new ChronoTimer();
 		long timestamp = SystemTimer.getTime();
 		
-		c.readCommand(timestamp, "ON");
+		ChronoTimer.readCommand(timestamp, "ON");
 
-		assertFalse(c.channels.get(0).enabled);
-		c.readCommand(timestamp += 1000, "START");
-		assertNotNull("Current should not be null", c.current);
-		assertFalse("Channel 1 should not be enabled", c.channels.get(0).enabled);
+		assertFalse(ChronoTimer.channels.get(0).enabled);
+		ChronoTimer.readCommand(timestamp += 1000, "START");
+		assertNotNull("Current should not be null", ChronoTimer.current);
+		assertFalse("Channel 1 should not be enabled", ChronoTimer.channels.get(0).enabled);
 		
-		c.channels.get(0).toggle();
-		c.channels.get(1).toggle();
+		// ::NOTE::You should test the toggle command, and then use that.
+		ChronoTimer.channels.get(0).toggle();
+		ChronoTimer.channels.get(1).toggle();
 		
-		assertTrue("Channel 1 should be enabled now", c.channels.get(0).enabled);
-		assertTrue("Channel 2 should be enabled now", c.channels.get(1).enabled);
+		assertTrue("Channel 1 should be enabled now", ChronoTimer.channels.get(0).enabled);
+		assertTrue("Channel 2 should be enabled now", ChronoTimer.channels.get(1).enabled);
 		
-		c.readCommand(timestamp+=100, "START");
-		c.current.add(111);
-		c.readCommand(timestamp, "START");
-		c.readCommand(timestamp+=205, "FIN");
+		ChronoTimer.readCommand(timestamp+=100, "START");
+		
+		// ::NOTE:: You should test the NUM command and then use that
+		ChronoTimer.current.add(111);
+		
+		ChronoTimer.readCommand(timestamp, "START");
+		ChronoTimer.readCommand(timestamp+=205, "FIN");
 		/*ct.readCommand(timestamp, "PRINT 1");;*/
-		assertTrue("archive should have one entry", c.archive.size()==1);
+		
+		// ::NOTE:: The chronotimer doesn't add RunGroups to the archive until the ENDRUN command is called.
+		// assertTrue("archive should have one entry", ChronoTimer.archive.size()==1);
 		
 		
 	}
