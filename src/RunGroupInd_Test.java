125ba8114426a6a08d7d530f87efea645573641c
Chris Harmon <cjharmon@uwm.edu>
2015-04-02 12:44:34 -0500

    Moved shared function tests into RunGroupShared_Test and out of RunGroupInd_Test. Still need to test accessors in RunGroupShared, and also need to test RunGroupGrp.

--- src/RunGroupInd_Test.java
+++ src/RunGroupInd_Test.java
@@ -17,41 +17,10 @@ public class RunGroupInd_Test {
 	 */
 	@Test
 	public void testConstructor() {
-		rg = new RunGroupInd();
-		assertEquals("RunGroup was not given correct run num", 1, rg.runNum);
-		assertEquals("RunGroup was not given correct default start channel", 1, rg.startChannel);
-		assertEquals("RunGroup was not given correct default finish channel", 2, rg.finishChannel);
-		
-		// Add a rungroup to the archive and make sure that the next run group's runNum is incremented.
-		ChronoTimer.archive.add(rg);
-		
-		rg = new RunGroupInd();
-		assertEquals("RunGroup was not given correct run num", 2, rg.runNum);
-	}
-	
-	/**
-	 * Tests that add correctly creates a run and adds it to the end of the start queue.
-	 */
-	@Test
-	public void testAdd() {
-		ChronoTimer.archive.clear();
-		rg = new RunGroupInd();
-		
-		// Make sure adding a run works.
-		rg.add(1);
-		assertEquals("No run was not added to the startqueue", 1, rg.startQueue.size());
-		assertEquals("Run was given the wrong bibNum", 1, rg.startQueue.peek().bibNum);
-		assertEquals("Run was given the wrong runNum", 1, rg.startQueue.peek().runNum);
-		assertEquals("Run was given the wrong state", "waiting", rg.startQueue.peek().state);
-		
-		// Make sure adding another run doesn't change who's next in line.
-		rg.add(2);
-		assertEquals("No run was not added to the startqueue", 2, rg.startQueue.size());
-		assertEquals("The next run in line changed", 1, rg.startQueue.peek().bibNum);
-		
-		// And make sure that the second run added was correct.
-		rg.startQueue.poll();
-		assertEquals("Run 2 was given the wrong bibNum", 2, rg.startQueue.peek().bibNum);
+		RunGroupInd rg = new RunGroupInd();
+		assertEquals("RunGroup was not given correct start channel", 1, rg.startChannel );
+		assertEquals("RunGroup was not given correct finish channel", 2, rg.finishChannel );
+		assertEquals("RunGroup was not given correct eventType", "IND", rg.eventType );
 	}
 	
 	/**
@@ -247,62 +216,5 @@ public class RunGroupInd_Test {
 		assertEquals("Run 1 was not given the correct state.", 1, rg.completedRuns.poll().bibNum);
 		assertEquals("Run 2 was not given the correct state.", "dnf", rg.completedRuns.peek().state);
 		assertEquals("Run 3 was not given the correct state.", 2, rg.completedRuns.poll().bibNum);
-	}
-	
-	/**
-	 * Tests that print (and Run.print()) correctly prints all runs from completedRuns, 
-	 * then the finishQueue, then the startQueue.
-	 */
-	@Test
-	public void testPrint() {
-		ChronoTimer.archive.clear();
-		rg = new RunGroupInd();
-		ChronoTimer.channels = new ArrayList<Channel>();
-		ChronoTimer.channels.add(new Channel(1));
-		ChronoTimer.channels.add(new Channel(2));
-		ChronoTimer.channels.get(0).toggle();
-		ChronoTimer.channels.get(1).toggle();
-		
-		// Set up a rungroup that will test all print locations.
-		rg.add(1);
-		rg.trigger(1, 42);
-		rg.trigger(2, 84); 	// Now there is one in the completedRuns with a finishTime
-		
-		rg.add(2);
-		rg.trigger(1, 88);
-		rg.dnf();			// Now there is one in the completedRuns with a dnf.
-		
-		rg.add(3);
-		rg.trigger(1, 9001);// Now there is one in finishQueue with state "inProgress"
-		
-		rg.add(4);			// And finally one in startQueue with state "waiting"
-		
-		Printer.log.clear();
-		rg.print();
-		assertEquals("Printer did not print correct number of lines", 1, Printer.log.size());
-	}
-	
-	@Test
-	public void testEnd() {
-		rg = new RunGroupInd();
-		ChronoTimer.channels = new ArrayList<Channel>();
-		ChronoTimer.channels.add(new Channel(1));
-		ChronoTimer.channels.get(0).toggle();
-		
-		rg.add(1);
-		rg.add(2);
-		rg.trigger(1, 0);
-		Printer.log.clear();
-	
-		// Make sure that runs both waiting and running are dnf'd.
-		rg.end();
-		assertEquals("Run was not placed in completedRuns.", 2, rg.completedRuns.size());
-		assertEquals("Run was not given the correct state.", "dnf", rg.completedRuns.poll().state);
-		assertEquals("Run was not given the correct state.", "dnf", rg.completedRuns.poll().state);
-		
-		// And make sure a message was printed.
-		assertEquals("No message was printed to the printer.", 2, Printer.log.size());
-		assertEquals("Incorrect message was printed to the printer.", "Bib #2 Did Not Finish", Printer.log.get(0));
-		assertEquals("Incorrect message was printed to the printer.", "Bib #1 Did Not Finish", Printer.log.get(1));
 	}
 }
