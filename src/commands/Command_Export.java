f041a78c6c46a082995c599c5d19e74d956281f0
Kevari <Kevari@Kevari-PC>
2015-04-19 17:20:51 -0400

    updated export command.

--- src/commands/Command_Export.java
+++ src/commands/Command_Export.java
@@ -27,8 +27,8 @@ public class Command_Export implements Command {
 
 	@Override
 	public void execute() {
-	//	this.exportToXML();
-		this.exportToJson();
+		this.exportToXML();
+		this.formatToJson();
 		this.updateURL();
 	}
 
@@ -130,7 +130,7 @@ public class Command_Export implements Command {
 
 	}
 
-	public void exportToJson(){
+	public void formatToJson(){
 		// Takes care of the case that there is no run that corresponds to the given run number
 		if( (ChronoTimer.getCurrent() == null || ChronoTimer.getCurrent().getRun() != runNum) &&
 				( runNum > ChronoTimer.getArchive().size() || runNum <= 0)) {
@@ -147,9 +147,6 @@ public class Command_Export implements Command {
 			group = ChronoTimer.getArchive().get(runNum-1);
 		}
 
-		String fileName = "RUN #" + runNum;
-		File outFile = new File(fileName);
-
 		String  item = "{\"event\":\"";
 
 		String runs = "\"run\":\"";
@@ -158,84 +155,74 @@ public class Command_Export implements Command {
 		String finish = "\"finish\":\"";
 		String elapsed = "\"elapsed\":\"";
 
+		String toJson = "[";
 
-		try {
-			BufferedWriter fw = new BufferedWriter(new FileWriter(outFile));
+		// Export completed runs.
+		if ( !group.getCompletedRuns().isEmpty() ) {
+			Iterator<Run> iterator = group.getCompletedRuns().iterator();
+			while ( iterator.hasNext() ) {
+				Run run = iterator.next();
 
-			String toJson = "[";
+				toJson += item + group.getEventType() + "\",";
+				toJson += runs + run.getRunNum() + "\",";
+				toJson += bib + run.getBibNum() + "\",";
+				toJson += start + SystemTimer.convertLongToString(run.getStartTime()) + "\",";
 
-			// Export completed runs.
-			if ( !group.getCompletedRuns().isEmpty() ) {
-				Iterator<Run> iterator = group.getCompletedRuns().iterator();
-				while ( iterator.hasNext() ) {
-					Run run = iterator.next();
-
-					toJson += item + group.getEventType() + "\",";
-					toJson += runs + run.getRunNum() + "\",";
-					toJson += bib + run.getBibNum() + "\",";
-					toJson += start + SystemTimer.convertLongToString(run.getStartTime()) + "\",";
-
-					if(run.getState().equals("dnf")){
-						toJson += finish + "DNF\",";
-						toJson += elapsed + "DNF\"},";
-					} else {
-						toJson += finish + SystemTimer.convertLongToString(run.getFinishTime()) + "\",";
-						toJson += elapsed + run.getElapsed() +"\"},";
-					}
-
-				}
-			}
-
-			// Export inProgress runs.
-			if ( !group.getFinishQueue().isEmpty() ) {
-				Iterator<Run> iterator = group.getFinishQueue().iterator();
-				while ( iterator.hasNext() ) {
-					Run run = iterator.next();
-
-					toJson += item + group.getEventType() + "\",";
-					toJson += runs + run.getRunNum() + "\",";
-					toJson += bib + run.getBibNum() + "\",";
-					toJson += start + SystemTimer.convertLongToString(run.getStartTime()) + "\",";
-					toJson += finish + "RUNNING\",";
-					toJson += elapsed + "RUNNING(" + run.getElapsed() + ")\"},";
-
-
+				if(run.getState().equals("dnf")){
+					toJson += finish + "DNF\",";
+					toJson += elapsed + "DNF\"},";
+				} else {
+					toJson += finish + SystemTimer.convertLongToString(run.getFinishTime()) + "\",";
+					toJson += elapsed + run.getElapsed() +"\"},";
 				}
 
 			}
-
-			// Export waiting runs.
-			if ( !group.getStartQueue().isEmpty() ) {
-				Iterator<Run> iterator = group.getStartQueue().iterator();
-				while ( iterator.hasNext() ) {
-					Run run = iterator.next();
-
-					toJson += item + group.getEventType() + "\",";
-					toJson += runs + run.getRunNum() + "\",";
-					toJson += bib + run.getBibNum() + "\",";
-					toJson += start + "WAITING\",";
-					toJson += finish +  "WAITING\",";
-					toJson += elapsed +  "WAITING\"},";
-				}
-
-			}	
-
-			toJson = toJson.substring(0, (toJson.length()-1));
-			toJson += "]";
-			inJson = toJson;
-			fw.write(toJson);
-			fw.close();
-		} catch(Exception e){//catch (IOException e) {
-			e.printStackTrace();
 		}
 
+		// Export inProgress runs.
+		if ( !group.getFinishQueue().isEmpty() ) {
+			Iterator<Run> iterator = group.getFinishQueue().iterator();
+			while ( iterator.hasNext() ) {
+				Run run = iterator.next();
+
+				toJson += item + group.getEventType() + "\",";
+				toJson += runs + run.getRunNum() + "\",";
+				toJson += bib + run.getBibNum() + "\",";
+				toJson += start + SystemTimer.convertLongToString(run.getStartTime()) + "\",";
+				toJson += finish + "RUNNING\",";
+				toJson += elapsed + "RUNNING(" + run.getElapsed() + ")\"},";
+
+
+			}
+
+		}
+
+		// Export waiting runs.
+		if ( !group.getStartQueue().isEmpty() ) {
+			Iterator<Run> iterator = group.getStartQueue().iterator();
+			while ( iterator.hasNext() ) {
+				Run run = iterator.next();
+
+				toJson += item + group.getEventType() + "\",";
+				toJson += runs + run.getRunNum() + "\",";
+				toJson += bib + run.getBibNum() + "\",";
+				toJson += start + "WAITING\",";
+				toJson += finish +  "WAITING\",";
+				toJson += elapsed +  "WAITING\"},";
+			}
+
+		}	
+
+		toJson = toJson.substring(0, (toJson.length()-1));
+		toJson += "]";
+		inJson = toJson;
 	}
 
 	public void updateURL() {
 		try {
-			//			URL site = new URL("http://harmonlab7.appspot.com/helloworld");
 			URL site = new URL("http://teambolt361.appspot.com/server");
 			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
+
 			conn.setDoOutput(true);
 			conn.setDoInput(true);
 			conn.setRequestMethod("POST");
