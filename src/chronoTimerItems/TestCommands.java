cbe422b4a7a5fbfd3410d4f360b5ec5db7a97650
Chris Harmon <cjharmon@uwm.edu>
2015-05-05 09:46:43 -0500

    Removed an unneeded import.

--- src/chronoTimerItems/TestCommands.java
+++ src/chronoTimerItems/TestCommands.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import junit.framework.Assert;
-
 import org.junit.Before;
 import org.junit.Test;
 
